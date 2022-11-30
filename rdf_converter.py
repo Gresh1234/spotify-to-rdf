32f5a6dd58815ab2301edaba0a25ae3a6f88399d
Elías Moreno <elias.moreno.ver@gmail.com>
2022-11-30 16:42:07 -0300

    Arreglado propertyChainAxiom

--- rdf_converter.py
+++ rdf_converter.py
@@ -1,21 +1,23 @@
-from rdflib import Graph, Literal, RDF, URIRef, BNode, Namespace #basic RDF handling
+'''ligma'''
 import ast
-import pandas as pd #for handling csv and csv contents
-from rdflib.namespace import XSD, RDFS, OWL #most common namespaces
 import urllib.parse #for parsing strings to URI's
+import pandas as pd #for handling csv and csv contents
+from rdflib import Graph, Literal, RDF, URIRef, Namespace #basic RDF handling
+from rdflib.graph import Collection
+from rdflib.namespace import XSD, RDFS, OWL #most common namespaces
 
 
 df = pd.read_csv("csv/tracks_genre.csv",sep=",",quotechar='"') #import csv file
 
-g = Graph(base="http://example.org/")
-root = Namespace("http://example.org/")
+g = Graph(base="http://example.org/#")
+root = Namespace("http://example.org/#")
 track = Namespace("http://example.org/tracks/")
 artist = Namespace("http://example.org/artists/")
 
 g.bind("track", track)
 g.bind("artist", artist)
 
-for i, row in df.head(100).iterrows():
+for i, row in df.head(10).iterrows():
     track_id = URIRef(track + urllib.parse.quote(row['id']))
 
     g.add((track_id, RDF.type, URIRef(root + "Track")))
@@ -23,12 +25,12 @@ for i, row in df.head(100).iterrows():
     g.set((track_id, URIRef(track + "popularity"), Literal(row['popularity'])))
     g.set((track_id, URIRef(track + "duration_ms"), Literal(row['duration_ms'])))
     g.set((track_id, URIRef(track + "explicit"), Literal(row['explicit'])))
-    
+
     artist_ids = ast.literal_eval(row["id_artists"])
     artist_names = ast.literal_eval(row["artists"])
 
-    for i in range(len(artist_ids)):
-        artist_id = URIRef(artist + urllib.parse.quote(artist_ids[i]))
+    for i, artist_i in enumerate(artist_ids):
+        artist_id = URIRef(artist + urllib.parse.quote(artist_i))
         artist_name = artist_names[i]
 
         g.add((track_id, URIRef(track + "hasArtist"), artist_id))
@@ -36,11 +38,11 @@ for i, row in df.head(100).iterrows():
         g.add((artist_id, RDF.type, URIRef(root + "Artist")))
         g.set((artist_id, URIRef(artist + "Name"), Literal(artist_name)))
         g.add((artist_id, URIRef(artist + "hasTrack"), track_id))
-        
-    g.add((track_id, URIRef(track + "releaseDate"), Literal(row['release_date'], datatype=XSD.gYear)))
+
+    g.add((track_id, URIRef(track + "releaseDate"), Literal(
+        row['release_date'], datatype=XSD.gYear)))
 
     g.add((track_id, URIRef(track + "hasGenre"), Literal(row['genre'])))
-
 
 g.add((URIRef(artist + "hasTrack"), RDFS.domain, URIRef(root + "Artist")))
 g.add((URIRef(artist + "hasTrack"), RDFS.range, URIRef(root + "Track")))
@@ -50,13 +52,11 @@ g.add((URIRef(artist + "hasArtist"), RDFS.range, URIRef(root + "Artist")))
 
 g.add((URIRef(artist + "hasArtist"), OWL.inverseOf, URIRef(artist + "hasTrack")))
 
-#g.add((URIRef(artist + "hasCollaboratedWith"), OWL.propertyChainAxiom, URIRef(artist + "hasTrack") , URIRef(artist + "hasArtist")))
+c = Collection(g, URIRef(root + "ligma"), [URIRef(artist + "hasTrack") , URIRef(
+    track + "hasArtist")])
+
+g.add((URIRef(artist + "hasCollaboratedWith"), OWL.propertyChainAxiom, URIRef(root + "ligma")))
 g.add((URIRef(artist + "hasCollaboratedWith"), RDF.type, OWL.IrreflexiveProperty))
 g.add((URIRef(artist + "hasCollaboratedWith"), RDF.type, OWL.SymmetricProperty))
-
-# g.add((URIRef(artist + "hasArtist"), ni idea, URIRef(artist + "hasCollaboratedWith")))
-
-
-
 
 print(g.serialize())
